31f338b2a1da5dd4f020cd767b0a318638af664e
Ming Gui <ming.gui@tum.de>
2022-06-24 13:28:47 +0000

    Remove unnecessary dependencies

--- data/kitti.py
+++ data/kitti.py
@@ -1,25 +1,13 @@
-import glob
-import random
 import os
 import numpy as np
-
 import torch
-
 from torch.utils.data import Dataset
 from PIL import Image
-import torchvision.transforms as transforms
-
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-
-from skimage.transform import resize
-
-import sys
 import os.path as osp
 import cv2
 
 
-KITTI_CLASSES = (  # always index 0
+KITTI_CLASSES = (
 'Car',
 'Van',
 'Truck',
